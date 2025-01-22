70b4729abda2339320ac989c617a7caf6bca1109
Jmelgar1014 <86273515+Jmelgar1014@users.noreply.github.com>
2025-01-22 11:52:00 -0500

    can now serve website

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import * as path from "path";
+import { fileURLToPath } from "url"; // Import this to resolve ES module paths
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 import "dotenv/config";
+
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
-
-// Create a single supabase client for interacting with your database
