40e9b92385d7deb409064cf5da3345945d1e69a9
Jmelgar1014 <86273515+Jmelgar1014@users.noreply.github.com>
2025-05-17 23:49:02 -0400

    fixing env issues

--- client/supaBaseData.js
+++ client/supaBaseData.js
@@ -1,8 +1,8 @@
+import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
-const supaBaseUrl = "https://eypcfuerwkprmxglvxyr.supabase.co";
+const supaBaseUrl = process.env.supaBaseUrl;
 
-const supaBaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5cGNmdWVyd2twcm14Z2x2eHlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0MDM0MDEsImV4cCI6MjA0OTk3OTQwMX0.zmE613jDERWczKF6UYlbG_z3RdGxzk1Px-OM-UXpcuo";
+const supaBaseKey = process.env.supaBaseKey;
 
 const supabase = createClient(supaBaseUrl, supaBaseKey);
 
