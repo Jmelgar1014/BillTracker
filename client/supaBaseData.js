8500efd500d437ae6f8a34b1ae72b317bb3c5ea6
Jmelgar1014 <86273515+Jmelgar1014@users.noreply.github.com>
2025-05-17 23:02:58 -0400

    removed some console logs

--- client/supaBaseData.js
+++ client/supaBaseData.js
@@ -1,9 +1,6 @@
 import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
 
-console.log(process.env.test);
-console.log(process.env.supaBaseKey);
-
 const supaBaseUrl = process.env.supaBaseUrl;
 
 const supaBaseKey = process.env.supaBaseKey;
